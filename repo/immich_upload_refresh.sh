6df869bc98e4b405b77e75ce1d60ec212097434f
Ruben <lqb@yomolo.com>
2023-12-04 01:23:08 +0100

    :

--- repo/immich_upload_refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-url=$(cat ~/.cloudbox_url)
-username=$(cat ~/.cloudbox_username)
-password=$(cat ~/.cloudbox_pwd)
-USER=$(whoami)
-owncloudcmd -u "${username}" -p "${password}" --non-interactive "/mnt/cloudclone/immich_upload/${USER}" "${url}" /immich
